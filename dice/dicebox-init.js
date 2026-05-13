c680f20b4814c1779a8040ede20f0aee06469c9b
Jonperi73 <jonperi73@gmail.com>
2026-05-12 23:11:18 -0300

    Change DiceBox import URL to use jsDelivr

--- dice/dicebox-init.js
+++ dice/dicebox-init.js
@@ -1,4 +1,4 @@
-import DiceBox from "https://unpkg.com/@3d-dice/dice-box@1.1.14/dist/dice-box.es.min.js";
+import DiceBox from "https://cdn.jsdelivr.net/npm/@3d-dice/dice-box@1.1.14/dist/dice-box.es.min.js";
 
 const diceBox = new DiceBox({
   assetPath: "./dice-box/",
